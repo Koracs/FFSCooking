75e0f363eddca7947e5290f89ec0f7a8c370b021
Felix <k12043299@students.jku.at>
2023-01-05 15:47:29 +0100

    attempt at sorting

--- frontend/src/pages/inventory/inventory.jsx
+++ frontend/src/pages/inventory/inventory.jsx
@@ -6,11 +6,11 @@ import React, {useEffect, useState} from "react";
 export default function Inventory() {
     const [ingredients, setIngredients] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [sortOrder, setSortOrder] = useState("oldest")
     const [formData, setFormData] = useState({
         ingredient: "",
         state: true
     });
-
     const {ingredient, state} = formData
     const onChange = (e) => {
         setFormData((prevState) => ({
@@ -19,49 +19,68 @@ export default function Inventory() {
         }))
     }
 
-    useEffect(() => {
-        async function getInventory() {
+    async function getInventory() {
+        setIsLoading(true);
+        const response = await fetch(`http://localhost:5000/api/inventory`)
 
-            const response = await fetch(`http://localhost:5000/api/inventory`)
-
-            if (!response.ok) {
-                const message = `An error occurred: ${response.statusText}`
-                window.alert(message)
-                return
-            }
-
-            const ingredients = await response.json();
-            setIngredients(ingredients)
-            setIsLoading(false);
+        if (!response.ok) {
+            const message = `An error occurred: ${response.statusText}`
+            window.alert(message)
+            return
         }
 
+        const ingredients = await response.json();
+        setIngredients(ingredients)
+        setIsLoading(false);
+    }
+
+    useEffect(() => {
         getInventory()
     }, [ingredients.length]);
+
+    useEffect(() => {
+        console.log(sortOrder)
+        sortInventory(sortOrder);
+    }, [sortOrder])
+
+    useEffect(() => {
+        console.log(ingredients)
+    }, [ingredients])
+
+    function sortInventory(sort) {
+        switch (sort) {
+            case 'newest':
+                setIngredients(ingredients.sort((a, b) => -1 * a.createdAt.localeCompare(b.createdAt)))
+                break;
+            case 'oldest':
+                setIngredients(ingredients.sort((a, b) => a.createdAt.localeCompare(b.createdAt)))
+                break;
+            case 'alphabetically':
+                setIngredients(ingredients.sort((a, b) => a.ingredient.localeCompare(b.ingredient)))
+                break;
+            default:
+                break;
+        }
+    }
 
 
     async function onSubmit(e) {
         e.preventDefault();
+        const newIngredient = {...formData};
 
-        const newRecipe = {...formData};
-
-        await fetch("http://localhost:5000/api/inventory", { //todo catch response
+        await fetch("http://localhost:5000/api/inventory", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify(newRecipe),
-        }).catch(error => { //todo: error message not showing
+            body: JSON.stringify(newIngredient),
+        }).catch(error => {
             window.alert(error);
             return;
         });
-        //todo: no confirmation showing
         setFormData({ingredient: "", state: true})
 
-
-        //todo this reloads all ingredients?
-        const newResponse = await fetch(`http://localhost:5000/api/inventory`)
-        const newIngredients = await newResponse.json();
-        setIngredients(newIngredients)
+        getInventory()
     }
 
     async function invertState(ingredient) {
@@ -71,44 +90,43 @@ export default function Inventory() {
             window.alert(message)
             return
         }
-        //todo this reloads all ingredients?
-        const newResponse = await fetch(`http://localhost:5000/api/inventory`)
-        const newIngredients = await newResponse.json();
-        setIngredients(newIngredients)
-
+        getInventory()
     }
+
 
     return (
         <>
-            <div style={{display: "flex", justifyContent: "space-around", alignItems: "center"}}>
-                <span style={{width: "15%"}}></span>
-                <h1 style={{width: "70%", textAlign: "center"}}>My Inventory</h1>
-                <span style={{width: "15%", textAlign: "right"}}>
+            {isLoading ? <div>Loading</div> : <>
+                <div style={{display: "flex", justifyContent: "space-around", alignItems: "center"}}>
+                    <span style={{width: "15%"}}></span>
+                    <h1 style={{width: "70%", textAlign: "center"}}>My Inventory</h1>
+                    <span style={{width: "15%", textAlign: "right"}}>
                     <Link to={`/inventory/edit`} className="button"> Edit Inventory </Link>
                 </span>
-            </div>
-            <div style={{textAlign: "right", padding: "1rem"}}>
-                <form style={{display:"inline"}} onSubmit={onSubmit}>
-                    <label htmlFor="ingredient">Add Ingredient: </label>
-                    <input
-                        type="text"
-                        id="ingredient"
-                        name="ingredient"
-                        value={ingredient}
-                        placeholder="Enter an ingredient"
-                        onChange={onChange}
-                    />
-                </form>
-                <label style={{paddingLeft: "2rem"}}>Sort: </label>
-                <select id="sort">
-                    <option value="oldest">Oldest First</option>
-                    <option value="newest">Newest First</option>
-                    <option value="alphabetically">A - Z</option>
-                </select>
-            </div>
-            <div>
-                <InventoryOverview ingredients={ingredients} invertStateHandler={invertState} />
-            </div>
+                </div>
+                <div style={{textAlign: "right", padding: "1rem"}}>
+                    <form style={{display: "inline"}} onSubmit={onSubmit}>
+                        <label htmlFor="ingredient">Add Ingredient: </label>
+                        <input
+                            type="text"
+                            id="ingredient"
+                            name="ingredient"
+                            value={ingredient}
+                            placeholder="Enter an ingredient"
+                            onChange={onChange}
+                        />
+                    </form>
+                    <label style={{paddingLeft: "2rem"}}>Sort: </label>
+                    <select onChange={(e) => setSortOrder(e.target.value)} id="sort">
+                        <option value="oldest">Oldest First</option>
+                        <option value="newest">Newest First</option>
+                        <option value="alphabetically">A - Z</option>
+                    </select>
+                </div>
+                <div>
+                    <InventoryOverview ingredients={ingredients} invertStateHandler={invertState}/>
+                </div>
+            </>}
         </>
     )
 }
--- frontend/src/pages/inventory/viewInventory.jsx
+++ frontend/src/pages/inventory/viewInventory.jsx
@@ -9,6 +9,10 @@ export default function ViewInventory({ingredients,invertStateHandler}) {
         invertStateHandler(ingredient);
     }
 
+    useEffect(()=>{
+        console.log("ingredients")
+    },[ingredients])
+
     return (
         <div>
             <ul className="overview">
