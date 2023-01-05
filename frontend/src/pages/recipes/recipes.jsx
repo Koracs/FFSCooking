5c270ca387b7adf7399fd06b8050bbcf06d3d2be
Sebastian <115575444+sewaUni@users.noreply.github.com>
2023-01-05 22:00:04 +0100

    change structure, fix footer position issue, add sorting function to recipe (refreshing does not work)

--- frontend/src/pages/recipes/recipes.jsx
+++ frontend/src/pages/recipes/recipes.jsx
@@ -4,38 +4,58 @@ import React, {useEffect, useState} from "react";
 
 export default function Recipes() {
     const [recipes, setRecipes] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [sortOrder, setSortOrder] = useState("oldest")
 
     useEffect(() => {
-            async function getRecipes() {
-                const response = await fetch(`http://localhost:5000/api/recipes`);
-
-                if (!response.ok) {
-                    const message = `An error occurred: ${response.statusText}`;
-                    window.alert(message);
-                    return;
-                }
-                const recipes = await response.json();
-                setRecipes(recipes);
-            }
-
+            console.log('useEffect: ')
+            console.log(recipes)
             getRecipes();
-            return;
         },
         [recipes.length]
     );
 
-    function sortRecipes(value) {
-        const test = recipes.sort((a, b) => a.name.localeCompare(b.name))
-        console.log(value);
-        console.log(value.value);
-        /*if(id === 1) console.log(1)
-        if(id === 2) console.log(2)
-        if(id === 3) console.log(3)*/
-        console.log("nix")
+    useEffect(() => {
+            console.log('useEffect: '+sortOrder)
+            sortRecipes(sortOrder)
+        },
+        [sortOrder]);
+
+    async function getRecipes() {
+        const response = await fetch(`http://localhost:5000/api/recipes`);
+
+        if (!response.ok) {
+            const message = `An error occurred: ${response.statusText}`;
+            window.alert(message);
+            return;
+        }
+        const recipes = await response.json();
+        setRecipes(recipes);
+        setIsLoading(false);
+    }
+
+    //https://dev.to/ramonak/react-how-to-dynamically-sort-an-array-of-objects-using-the-dropdown-with-react-hooks-195p
+    function sortRecipes(sortOrder) {
+        // eslint-disable-next-line default-case
+        switch (sortOrder) {
+            case 'oldest':
+                console.log("oldest");
+                setRecipes(recipes.sort((o1, o2) => o1.createdAt.localeCompare(o2.createdAt)));
+                break;
+            case 'newest':
+                console.log("newest");
+                setRecipes(recipes.sort((o1, o2) => o2.createdAt.localeCompare(o1.createdAt)));
+                break;
+            case 'alphabetically':
+                console.log("alphabetically");
+                setRecipes(recipes.sort((o1, o2) => o1.name.localeCompare(o2.name)));
+                break;
+        }
     }
 
     return (
         <>
+            {isLoading ? <div>Loading</div> : <>
             <div style={{display: "flex", justifyContent: "space-around", alignItems: "center"}}>
                 <span style={{width: "15%"}}></span>
                 <h1 style={{width: "70%", textAlign: "center"}}>Recipe Overview</h1>
@@ -53,7 +73,7 @@ export default function Recipes() {
                     placeholder="Find items by name..."
                 />
                 <label style={{paddingLeft: "2rem"}}>Sort: </label>
-                <select id="sort">
+                <select id="sort" onChange={(e) => sortRecipes(e.target.value)} >
                     <option value="oldest">Oldest First</option>
                     <option value="newest">Newest First</option>
                     <option value="alphabetically">A - Z</option>
@@ -62,8 +82,7 @@ export default function Recipes() {
             <div>
                 <RecipeOverview recipes={recipes}/>
             </div>
-
-
+            </>}
         </>
     )
 }
